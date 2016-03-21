f3d6c8366e9f76e9836dcdcb79980bd3511ae692
papeixoto <pazevedopeixoto@gmail.com>
2016-03-21 16:41:39 +0000

    New tests EclEmma 83% PIT 54%

--- src/maze/test/testSomeRandomBehavior.java
+++ src/maze/test/testSomeRandomBehavior.java
@@ -17,40 +17,100 @@ public class testSomeRandomBehavior {
 			{'X', 'E', ' ', ' ', 'X'},
 			{'X', 'X', 'X', 'X', 'X'}};
 
-	/*		char [][] m2 =
-			{{'X', 'X', 'X', 'X', 'X', 'X', 'X'},
-			 {'X', 'H', 'X', ' ', ' ', ' ', 'S'},
-			 {'X', ' ', 'X', ' ', 'D', ' ', 'X'},
-			 {'X', ' ', 'X', ' ', ' ', ' ', 'X'},
-			 {'X', ' ', 'X', ' ', ' ', ' ', 'X'},
-			 {'X', 'E', 'X', ' ', ' ', ' ', 'X'},
-			 {'X', 'X', 'X', 'X', 'X', 'X', 'X'}}*/		
+	char [][] m2 =
+		{{'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+				{'X', 'H', 'X', ' ', ' ', ' ', 'S'},
+				{'X', ' ', 'X', ' ', 'D', ' ', 'X'},
+				{'X', ' ', 'X', ' ', ' ', ' ', 'X'},
+				{'X', ' ', 'X', ' ', ' ', ' ', 'X'},
+				{'X', 'E', 'X', ' ', ' ', ' ', 'X'},
+				{'X', 'X', 'X', 'X', 'X', 'X', 'X'}};		
 
 
 	@Test
 	public void testModesMedium() {
-		Maze maze=new Maze(m1);
-		boolean move=false;
-		maze.updateMedium('s');
-		if(m1[1][3]==maze.getDragons().get(0).getSymbol())
-		{
-			move=true;
-		}
-		if(m1[2][3]==maze.getDragons().get(0).getSymbol())
-		{
-			move=true;
-		}
-		if(m1[1][2]==maze.getDragons().get(0).getSymbol())
-		{
-			move=true;
-		}
-		AssertEquals(true,move);
+		boolean noMove = false, moveUp=false, moveDown = false, moveLeft=false, moveRight=false;
+		Maze maze=new Maze(m2);
 
-	}
-	private void AssertEquals(boolean b, boolean move) {
-		// TODO Auto-generated method stub
 
+
+		while(!noMove || !moveUp || !moveDown || !moveLeft || !moveRight){
+			int x=maze.getDragons().get(0).getX();
+			int y =maze.getDragons().get(0).getY();
+			maze.updateMedium('s');
+			char[][] board = maze.getBoard();
+			if(board[x][y]==maze.getDragons().get(0).getSymbol())
+			{
+				noMove=true;
+
+			}
+			if(board[x-1][y]==maze.getDragons().get(0).getSymbol())
+			{
+				moveUp=true;
+
+			}
+			if(board[x+1][y]==maze.getDragons().get(0).getSymbol())
+			{
+				moveDown=true;
+
+			}
+			if(board[x][y-1]==maze.getDragons().get(0).getSymbol())
+			{
+				moveLeft=true;
+
+			}
+			if(board[x][y+1]==maze.getDragons().get(0).getSymbol())
+			{
+				moveRight=true;
+
+			}
+		}
+		assertEquals(true,noMove && moveUp && moveDown && moveLeft && moveRight);
 	}
+	
+	@Test
+	public void testModesHard() {
+		boolean noMove = false, moveUp=false, moveDown = false, moveLeft=false, moveRight=false, changeState = false;
+		Maze maze=new Maze(m2);
+
+
+
+		while(!noMove || !moveUp || !moveDown || !moveLeft || !moveRight || !changeState){
+			int x=maze.getDragons().get(0).getX();
+			int y =maze.getDragons().get(0).getY();
+			char c=maze.getDragons().get(0).getSymbol();
+			maze.updateHard('s');
+			char[][] board = maze.getBoard();
+			if(board[x][y]==maze.getDragons().get(0).getSymbol())
+			{
+				noMove=true;
+				if(board[x][y]!=c) changeState=true;
+
+			}
+			if(board[x-1][y]==maze.getDragons().get(0).getSymbol())
+			{
+				moveUp=true;
+
+			}
+			if(board[x+1][y]==maze.getDragons().get(0).getSymbol())
+			{
+				moveDown=true;
+
+			}
+			if(board[x][y-1]==maze.getDragons().get(0).getSymbol())
+			{
+				moveLeft=true;
+
+			}
+			if(board[x][y+1]==maze.getDragons().get(0).getSymbol())
+			{
+				moveRight=true;
+
+			}
+		}
+		assertEquals(true,noMove && moveUp && moveDown && moveLeft && moveRight && changeState);
+	}
+
 	@Test
 	public void testHardSleepingDragon(){
 		Maze maze=new Maze(m1);
@@ -58,11 +118,11 @@ public class testSomeRandomBehavior {
 		maze.getDragons().get(0).setAwake(false);
 		//acorda tem que estar no mesmo sitio
 		maze.updateHard('s');
-		if(m1[1][3]==maze.getDragons().get(0).getSymbol())
+		if(m1[1][3]=='D' || m1[1][3]=='d')
 			move=true;
 		assertEquals(true,move);
 	}
-/*
+	/*
 	@Test
 	public void testHardAwakeDragon(){
 		Maze maze=new Maze(m1);
@@ -85,9 +145,9 @@ public class testSomeRandomBehavior {
 		{
 			move=true;
 		}
-		
+
 		assertEquals(true,move);
 	}
-	*/
+	 */
 }
 
